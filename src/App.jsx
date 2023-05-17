36f088966638e0d51a30deff47a964b12318ec0a
kevinpa1000 <kevinkantonap@gmail.com>
2023-05-18 01:13:59 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+// eslint-disable-next-line no-unused-vars
+import { Typography } from "@mui/joy";
+import {Outlet} from "react-router-dom";
+
+function App() {
+  return (
+<>
+  <Outlet />
+</>  
+);
+}
+
+export default App;
